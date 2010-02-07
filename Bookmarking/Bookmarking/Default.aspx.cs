8532ae9bf7acbdd4d8a28a6d1376e4f7eaf924c4
Alan Christensen <alan@christensen.org.nz>
2010-02-07 18:49:51 +0000

    redirect from / to /home

--- Bookmarking/Bookmarking/Default.aspx.cs
+++ Bookmarking/Bookmarking/Default.aspx.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace Bookmarking
 {
@@ -11,7 +6,7 @@ namespace Bookmarking
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            Response.Redirect("/home");
         }
     }
 }
